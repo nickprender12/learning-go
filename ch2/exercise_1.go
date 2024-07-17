27ddff23b582b026acbffef2098c182791b2cfde
Nicholas Prendergast <nickprender@gmail.com>
2024-07-17 13:34:38 -0700

    second commit

---
+++ ch2/exercise_1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var i int = 20
+	// var f float64 = float64(i)
+	// fmt.Println(i)
+	// fmt.Println(f)
+
+	const value
+	var i int = value
+	var f float64 = value
+	fmt.Println(i)
+	fmt.Println(f)
+}
---
+++ ch2/go.mod
@@ -0,0 +1,3 @@
+module ch2_exercises
+
+go 1.21.6
